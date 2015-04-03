6eecf6007f26f414527b6bf4e5ca7d19e21f7f6a
Mark <markseanabbott@gmail.com>
2015-04-02 17:24:27 -0700

    quick fix

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,5 +1,5 @@
 angular.module('app').controller('mvNavBarLoginCtrl', function($scope){
   $scope.signin=function(username,password){
-    console.log("I am not done yet");
+    console.log("I am not done yet - your login is:" + username + " your password is - " + password);
   }
 })
